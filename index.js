79826dd1e520c5683772c7a9bf7dadcb752fbe01
mirage <mirage.kamran.pkt@gmail.com>
2020-06-13 16:52:23 -0700

    morgan added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 const express = require("express");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 // const noInsomnia = require('./middleware/no-insomnia')
 const logger = require("./middleware/logger");
 const userRouter = require("./users/userRouter");
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 server.use(express.json());
 // server.use(noInsomnia())
-// server.use(morgan("combined"));
+server.use(morgan("combined"));
 
 server.use(logger('long'))
 
