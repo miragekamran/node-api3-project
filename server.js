5c199d69887a100b6b9df46cb56983fa1cb5475e
mirage <mirage.kamran.pkt@gmail.com>
2020-06-13 14:56:07 -0700

    MVP met

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+const UserRouter = require("./users/userRouter");
+
+server.use(express.json());
+
+server.use("/api/users", UserRouter);
+
+server.get("/", (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+// function logger(req, res, next) {}
 
 module.exports = server;
